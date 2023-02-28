a0267ec2db0227bec720e29ed95f34d0fbdae343
Sanjana Senthil <saa.tnsw@gmail.com>
2023-02-28 09:33:27 +0000

    Completed exercises Part01_11.VariousVariables - Part01_20.MultiplicationFormula

---
+++ Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+
+        System.out.println("Give a string: ");
+        String string = scan.nextLine();
+        System.out.println("Give an integer:");
+        int integer = Integer.parseInt(scan.nextLine());
+        System.out.println("Give a double: ");
+        double doubleNumber = Double.parseDouble(scan.nextLine());
+        System.out.println("Give a boolean: ");
+        boolean bool = Boolean.parseBoolean(scan.nextLine());
+ 
+        System.out.println("You gave the string " + string);
+        System.out.println("You gave the integer " + integer);
+        System.out.println("You gave the double " + doubleNumber);
+        System.out.println("You gave the boolean " + bool);
+    }
+}
---
+++ Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MultiplicationFormula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+    }
+}
